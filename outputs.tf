e26b995121f5153840b0a1dc4a8f9f00bbf705a3
joestack <joern@joern-online.de>
2022-12-12 11:08:17 +0100

    additional outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,16 @@
-output "random_id" {
+output "random_id_dec" {
   value = random_id.rnd.dec
 }
+output "random_id_id" {
+  value = random_id.rnd.id
+}
+output "random_id_b64" {
+  value = random_id.rnd.b64_std
+}
+output "random_id_b64url" {
+  value = random_id.rnd.b64_url
+}
+output "random_id_hex" {
+  value = random_id.rnd.hex
+}
+
